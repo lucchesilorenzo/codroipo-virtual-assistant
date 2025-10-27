b47fdf87109a729a555b599eecb150cfa5159ab9
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-10-27 21:59:59 +0100

    feat: add main entry point for running scraping service scripts

---
+++ src/scripts/index.ts
@@ -0,0 +1,16 @@
+import { services } from "./services";
+
+async function main() {
+  for (const { service, buildFaq } of services) {
+    console.log(`=> Inizio scraping: ${service.name}`);
+
+    const data = await service();
+    await buildFaq(data);
+
+    console.log(`=> Completato: ${service.name}`);
+  }
+
+  console.log("Tutti gli scraping sono stati completati con successo.");
+}
+
+main();
