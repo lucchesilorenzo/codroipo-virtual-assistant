42b5922afc552ccd744b79390feef9fe7fa4ccc6
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-10-27 23:07:57 +0100

    feat: add "Cambio Nome e Cognome" service to services entry point

--- src/scripts/services/index.ts
+++ src/scripts/services/index.ts
@@ -1,6 +1,15 @@
 import { buildCartaIdentitaElettronicaFAQ } from "@/scripts/faq-builders/build-cie-faq";
+import { buildCambioNomeECognomeFAQ } from "../faq-builders/build-cambio-nome-e-cognome-faq";
+import { scrapeCambioNomeECognome } from "./scrape-cambio-nome-e-cognome";
 import { scrapeCIE } from "./scrape-cie";
 
 export const services = [
-  { service: scrapeCIE, buildFaq: buildCartaIdentitaElettronicaFAQ },
+  {
+    service: scrapeCIE,
+    buildFaq: buildCartaIdentitaElettronicaFAQ,
+  },
+  {
+    service: scrapeCambioNomeECognome,
+    buildFaq: buildCambioNomeECognomeFAQ,
+  },
 ];
