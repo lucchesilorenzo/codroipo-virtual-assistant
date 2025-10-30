6e3272d42731020357dab43c6cd3bd46c7d38f3d
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-10-30 19:42:50 +0100

    feat: add createLog helper function

---
+++ src/utils/create-log.ts
@@ -0,0 +1,9 @@
+import fs from "fs/promises";
+import path from "path";
+
+export async function createLog(data: string, dir: string, fileName: string) {
+  await fs.mkdir(dir, { recursive: true });
+
+  const filePath = path.join(dir, `${fileName}.log`);
+  await fs.appendFile(filePath, data);
+}
