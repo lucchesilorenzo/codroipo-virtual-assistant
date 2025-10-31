a683acedf736c837b44bd70eca2412b562804604
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2025-10-31 16:39:50 +0100

    refactor: enhance webhook handling for scheduled appointments

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
+import { format } from "date-fns";
 import express, { Request, Response } from "express";
+import { createJSON } from "./utils/create-json";
 import { createLog } from "./utils/create-log";
 
 const app = express();
@@ -11,25 +13,34 @@ app.post("/webhook", async (req: Request, res: Response) => {
   if (message.type === "end-of-call-report") {
     const messages = message.artifact?.messages || [];
 
-    for (const msg of messages) {
+    for (const message of messages) {
       if (
-        msg.role === "tool_call_result" &&
-        msg.name === "schedule_appointment"
+        message.role === "tool_call_result" &&
+        message.name === "schedule_appointment"
       ) {
-        const appointment = JSON.parse(msg.result);
-        const formattedDate = new Intl.DateTimeFormat("it-IT", {
-          day: "2-digit",
-          hour: "2-digit",
-          minute: "2-digit",
-          year: "numeric",
-          month: "2-digit",
-        }).format(new Date(appointment.start.dateTime));
+        const parsedAppointment = JSON.parse(message.result);
+
+        const formattedDate = format(
+          new Date(parsedAppointment.start.dateTime),
+          "dd/MM/yyyy, HH:mm"
+        );
+
+        const appointment = {
+          summary: parsedAppointment.summary,
+          htmlLink: parsedAppointment.htmlLink,
+          start: {
+            dateTime: parsedAppointment.start.dateTime,
+            timeZone: parsedAppointment.start.timeZone,
+          },
+        };
 
         await createLog(
-          `\nPrenotazione effettuata con successo!\nServizio: ${appointment.summary}\nData/Ora: ${formattedDate}\nLink: ${appointment.htmlLink}\n---------------------------`,
+          `Prenotazione effettuata con successo!\nServizio: ${appointment.summary}\nData/Ora: ${formattedDate}\nLink: ${appointment.htmlLink}\n-----------------\n`,
           "./src/logs",
-          "prenotazioni"
+          "prenotazione"
         );
+
+        await createJSON(appointment, "./src/logs", "prenotazione");
       }
     }
   }
